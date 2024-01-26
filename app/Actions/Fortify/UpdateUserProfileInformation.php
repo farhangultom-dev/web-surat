c3d3bdfa4f2698fe4f5b7efb0c9214d5696fc875
Muhammad Farhan Gultom <pitjarusfarhan@outlook.com>
2024-01-26 10:53:57 +0700

    update user information

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -20,6 +20,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
+            'nik' => ['required', 'string','unique:users'],
             'photo' => ['nullable', 'mimes:jpg,jpeg,png', 'max:1024'],
         ])->validateWithBag('updateProfileInformation');
 
@@ -34,6 +35,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
             $user->forceFill([
                 'name' => $input['name'],
                 'email' => $input['email'],
+                'nik' => $input['nik'],
             ])->save();
         }
     }
--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -59,6 +59,12 @@
             <x-input-error for="name" class="mt-2" />
         </div>
 
+        <div class="col-span-6 sm:col-span-4">
+            <x-label for="name" value="{{ __('Nik') }}" />
+            <x-input id="nik" type="text" class="mt-1 block w-full" wire:model="state.nik" required autocomplete="nik" />
+            <x-input-error for="nik" class="mt-2" />
+        </div>
+
         <!-- Email -->
         <div class="col-span-6 sm:col-span-4">
             <x-label for="email" value="{{ __('Email') }}" />
